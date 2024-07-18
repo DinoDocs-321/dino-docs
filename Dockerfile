cfed97f96535a119a03d2e3ee5b2df9240c097f4
soham-verma <sohamverma26@gmail.com>
2024-07-18 15:36:38 +1000

    docker containers added

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.12-slim
+
+WORKDIR /usr/src/app
+
+COPY . .
+
+RUN pip install -r requirements.txt
+
+EXPOSE 8000
+
+ENV PYTHONUNBUFFERED=1
+
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- dino/settings.py
+++ dino/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = "django-insecure-l$5$msnhqn3*hd(!g%xe0r^qk303v@ky@azm6#w&v(%$xm#apl
 # DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+  'localhost', 
+  '127.0.0.1', 
+  '0.0.0.0'
+]
 
 
 # Application definition
