b1e92e51fb7e8023cec5a2b5f5585d41b40e2df8
20977 <terence.yuanz@gmail.com>
2024-07-24 19:11:54 +1000

    Main Added react app inside 'dinodoc' by creating 'dinoreact' and 'reactapi' folder, changed settings and urls in 'dino' folder

--- dinodoc/dino/urls.py
+++ dinodoc/dino/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('dinoapp.urls')),
     # path('myapp/', include('myapp.urls'))
+    path("reactapi/", include('reactapi.urls'))
 ]
