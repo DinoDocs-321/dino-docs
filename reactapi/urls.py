8108cc31319749084c741a7a8a1bb79507434380
Lelton99 <111098398+Lelton99@users.noreply.github.com>
2024-10-03 20:39:37 +1000

    Update urls.py

--- reactapi/urls.py
+++ reactapi/urls.py
@@ -20,16 +20,11 @@ urlpatterns = [
     path('convert/', ConvertJsonToBson.as_view(), name='convert-json-schema-to-bson'),
     path('save-schema/', save_user_schema, name='save-schema'),
 
-
-
     #Generator paths
     path('data-types/', DataTypeList.as_view(), name='data-types'),
     path('generate-documents/', GenerateDocumentView.as_view(), name='generate_documents'),
     
     path('validate-json-file/', ValidateJsonTextView.as_view(), name='validate_json_file'),
     path('validate-json-text/', ValidateJsonTextView.as_view(), name='validate_json_text'),
-
-
-
 ]
 
